b59f45ce571e6d4476fa4f13cb6a766a2528b90b
senthil-lister <senthilkumar.c@listertechnologies.com>
2016-05-19 11:56:18 +0530

    Add endpoint to receive POST request and dump body into log

--- src/main/java/com/listertechnologies/occgf/web/CustomerController.java
+++ src/main/java/com/listertechnologies/occgf/web/CustomerController.java
@@ -2,6 +2,10 @@ package com.listertechnologies.occgf.web;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.http.client.ClientProtocolException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,11 +26,23 @@ public class CustomerController {
         System.out.println("Customer: " + customerDetail);
         return customerDetail.toString();
     }
-    
+
     @RequestMapping(value = "/test")
     @ResponseBody
     public String test() throws ClientProtocolException, IOException {
         new CustomerService().sendCustomerData();
+        return "Success";
+    }
+
+    @RequestMapping(value = "/dump", method = RequestMethod.POST)
+    @ResponseBody
+    public String dump(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String requestBody = IOUtils.toString(request.getInputStream(), "UTF-8");
+            System.out.println(requestBody);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "Success";
     }
 
